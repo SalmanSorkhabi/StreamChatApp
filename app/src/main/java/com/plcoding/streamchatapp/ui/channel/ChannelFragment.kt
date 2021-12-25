d8c72d4b6783bbe06abcaf48e5b9664b1397402d
salman sorkhabi <salman.sorkhabi511@gmail.com>
2021-12-25 15:05:24 +0330

    first commit

---
+++ app/src/main/java/com/plcoding/streamchatapp/ui/channel/ChannelFragment.kt
@@ -0,0 +1,94 @@
+package com.plcoding.streamchatapp.ui.channel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
+import com.plcoding.streamchatapp.R
+import com.plcoding.streamchatapp.databinding.FragmentChannelBinding
+import com.plcoding.streamchatapp.ui.BindingFragment
+import com.plcoding.streamchatapp.util.navigateSafely
+import dagger.hilt.android.AndroidEntryPoint
+import io.getstream.chat.android.client.models.Filters
+import io.getstream.chat.android.ui.channel.list.header.viewmodel.ChannelListHeaderViewModel
+import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
+import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
+import kotlinx.coroutines.flow.collect
+import java.util.logging.Filter
+
+@AndroidEntryPoint
+class ChannelFragment : BindingFragment<FragmentChannelBinding>() {
+
+    private val viewModel: ChannelViewModel by activityViewModels()
+
+    override val bindingInflater: (LayoutInflater) -> ViewBinding
+        get() = FragmentChannelBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val user = viewModel.getUser()
+        if (user == null) {
+            findNavController().popBackStack()
+            return
+        }
+
+        val factory = ChannelListViewModelFactory(
+            filter = Filters.and(
+                Filters.eq("type","messaging")
+            ),
+            sort = ChannelListViewModel.DEFAULT_SORT,
+            limit = 30
+        )
+        val channelListViewModel:ChannelListViewModel by viewModels { factory }
+        val channelListHeaderViewModel:ChannelListHeaderViewModel by viewModels()
+        channelListViewModel.bindView(binding.channelListView,viewLifecycleOwner)
+        channelListHeaderViewModel.bindView(binding.channelListHeaderView,viewLifecycleOwner)
+
+        binding.channelListHeaderView.setOnUserAvatarClickListener{
+            viewModel.logout()
+            findNavController().popBackStack()
+        }
+
+        binding.channelListHeaderView.setOnActionButtonClickListener{
+            findNavController().navigateSafely(
+                R.id.action_channelFragment_to_createChannelDialog
+            )
+        }
+
+        binding.channelListView.setChannelItemClickListener{channel ->
+            findNavController().navigateSafely(
+                R.id.action_channelFragment_to_chatFragment,
+                Bundle().apply { putString("channelId",channel.cid) }
+            )
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.createChannelEvent.collect { event ->
+                when(event){
+                    is ChannelViewModel.CreateChannelEvent.Error -> {
+                        Toast.makeText(
+                            requireContext(),
+                            event.error,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    is ChannelViewModel.CreateChannelEvent.Success -> {
+                        Toast.makeText(
+                            requireContext(),
+                            R.string.channel_created,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/plcoding/streamchatapp/ui/channel/CreateChannelDialog.kt
@@ -0,0 +1,48 @@
+package com.plcoding.streamchatapp.ui.channel
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.plcoding.streamchatapp.R
+import com.plcoding.streamchatapp.databinding.DialogChannelNameBinding
+
+class CreateChannelDialog : DialogFragment() {
+
+    private var _binding: DialogChannelNameBinding? = null
+    private val binding: DialogChannelNameBinding
+        get() = _binding!!
+
+    private val viewModel: ChannelViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        _binding = DialogChannelNameBinding.inflate(layoutInflater)
+        return MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.choose_channel_name)
+            .setView(binding.root)
+            .setPositiveButton(R.string.create) { _, _ ->
+                viewModel.createChannel(binding.etChannelName.text.toString())
+            }
+            .setNegativeButton(R.string.cancel){dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .create()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
